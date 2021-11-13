36502cef3fcce67a051ed864e090c1b1959f797f
Mpk320 <mpkasunick@gmail.com>
2021-11-13 06:15:11 -0500

    Loop works, make it more efficient and added notes

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import random
 
-e = input("Press amy key to get a random number: ")
-rand = random.randint(0, 100)
-print(rand)
+
+# generates number
+def generator():
+    rand = random.randint(0, 100)
+    print(rand)
+
+
+# gives option to quit or generate new number
+def redo():
+    cont = input("Press n to quit: ")
+    if cont != "n":
+        beginning()
+    else:
+        print("Have a nice day!")
+
+
+# allows loop to work
+def beginning():
+    generator()
+    redo()
+
+
+beginning()
